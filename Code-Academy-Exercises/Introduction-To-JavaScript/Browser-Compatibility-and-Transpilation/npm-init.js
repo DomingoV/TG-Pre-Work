550a4e816a00995b35bfc3e90762c7a8ef2cdea9
Domingo Vallari <domingovallari@gmail.com>
2018-10-05 11:04:33 -0500

    creating files in terminal

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Browser-Compatibility-and-Transpilation/npm-init.js
@@ -0,0 +1,15 @@
+// creates file
+//$ ls
+src
+$ npm init package.json
+This utility will walk you through creating a package.json file.
+It only covers the most common items, and tries to guess sensible de
+faults.
+
+See `npm help json` for definitive documentation on these fields
+and exactly what they do.
+
+Use `npm install <pkg> --save` afterwards to install a package and
+save it as a dependency in the package.json file.
+
+Press ^C at any time to quit.
