0c15768e3a50e00b120b34475b5f9ec08fc150da
Domingo Vallari <domingovallari@gmail.com>
2018-10-05 09:50:09 -0500

    Model

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Classes/Inheritance-1.js
@@ -0,0 +1,45 @@
+class Cat {
+  constructor(name, usesLitter) {
+    this._name = name;
+    this._usesLitter = usesLitter;
+    this._behavior = 0;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get usesLitter() {
+    return this._usesLitter;
+  }
+
+  get behavior() {
+    return this._behavior;
+  }
+
+  incrementBehavior() {
+    this._behavior++;
+  }
+}
+
+class Animal {
+  constructor(name) {
+    this._name = name;
+    this._behavior = 0;class Animal {
+  constructor(name) {
+    this._name = name;
+    this._behavior = 0;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get behavior() {
+    return this._behavior;
+  }
+
+  incrementBehavior() {
+    this._behavior++;
+  }
+}
