52fde7c1a52e56cd6f5244e2949a53e557beffb7
Domingo Vallari <domingovallari@gmail.com>
2018-10-05 09:15:01 -0500

    object containing properties and methods of a class

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Classes/Instance.js
@@ -0,0 +1,10 @@
+// object containing properties and methods of a class
+class Surgeon {
+  constructor(name, department) {
+    this.name = name;
+    this.department = department;
+  }
+}
+
+const surgeonCurry = new Surgeon('Curry', 'Cardiovascular');
+const surgeonDurant = new Surgeon('Durant', 'Orthopedics');
