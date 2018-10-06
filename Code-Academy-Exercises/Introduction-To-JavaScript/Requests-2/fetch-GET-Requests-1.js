dfb26be6f85dbf9afd9819924f564ca985ddb57e
Domingo Vallari <domingovallari@gmail.com>
2018-10-06 13:40:24 -0500

    fetch() GET

--- Code-Academy-Exercises/Introduction-To-JavaScript/Requests-2/fetch-GET-Requests-1.js
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Requests-2/fetch-GET-Requests-1.js
@@ -1,10 +1,11 @@
 //fetch() GET
 fetch('http://api-to-call.com/endpoint'). then(response => {
   if(response.ok) {
-    return respons.json();
+    return response.json();
   }
   throw new Error('Request failed!');
-}, networkError _> console.log(networkError.message)
-).then(jsonResponse => {
+}, networkError => {
+  console.log(networkError.message)
+}).then(jsonResponse => {
   // code to execute with jsonResponse
 });
