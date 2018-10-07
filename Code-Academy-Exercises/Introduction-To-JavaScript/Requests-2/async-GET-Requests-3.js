3360c84be9516ff9ab8f370d74ead2dc5b055f95
Domingo Vallari <domingovallari@gmail.com>
2018-10-06 19:37:03 -0500

    async GET await

--- Code-Academy-Exercises/Introduction-To-JavaScript/Requests-2/async-GET-Requests-3.js
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Requests-2/async-GET-Requests-3.js
@@ -1,34 +1,35 @@
 // Information to reach API
-const url = 'https://api.datamuse.com/words';
-const queryParams = '?sl=';
+const url = 'https://api.datamuse.com/words?';
+const queryParams = 'rel_jja=';
 
-// Selects page elements
+// Selecting page elements
 const inputField = document.querySelector('#input');
 const submit = document.querySelector('#submit');
 const responseField = document.querySelector('#responseField');
 
 // AJAX function
-const getSuggestions = () => {
+// Code goes here
+const getSuggestions = async () => {
   const wordQuery = inputField.value;
   const endpoint = `${url}${queryParams}${wordQuery}`;
-
-  fetch(endpoint).then(response => {
-    if (response.ok) {
-      return response.json();
+  try {
+    const response = await fetch(endpoint);
+    if(response.ok){
+      const jsonResponse = await response.json();
+      renderResponse(jsonResponse);
     }
-    throw new Error('Request failed!');
-  }, networkError => {
-    console.log(networkError.message)
-  })
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-// Clears previous results and display results to webpage
+// Clear previous results and display results to webpage
 const displaySuggestions = (event) => {
   event.preventDefault();
   while(responseField.firstChild){
     responseField.removeChild(responseField.firstChild);
   }
   getSuggestions();
-};
+}
 
 submit.addEventListener('click', displaySuggestions);
